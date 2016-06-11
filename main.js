e4f46e1879cfdafd3e711a832d7caaf64caad821
Adan Santos <santos.adaan@gmail.com>
2016-06-11 19:00:02 -0300

    Aula 7 - Interfaces para classes

--- main.js
+++ main.js
@@ -1,14 +1,49 @@
-var add;
-add = function (varA, varB) {
-    return true;
-};
-var getColor;
-getColor = function (codeColor, title) {
-    if (title)
-        return { codeColor: codeColor, title: title };
-    return { codeColor: codeColor };
-};
-console.log(getColor("#fff", "White"));
-var a;
-a = ["test", 65];
-console.log(a);
+var Mytext = (function () {
+    function Mytext(valueTxt) {
+        this.valueTxt = valueTxt;
+    }
+    ;
+    Mytext.prototype.countTxt = function () {
+        return this.valueTxt.length;
+    };
+    ;
+    return Mytext;
+}());
+var t = new Mytext("Adan Recarlos...");
+console.log(t.countTxt());
+var Mytext2 = (function () {
+    function Mytext2(valueTxt) {
+        this.valueTxt = valueTxt;
+    }
+    ;
+    Mytext2.prototype.countTxt = function () {
+        return this.valueTxt.length;
+    };
+    ;
+    Mytext2.prototype.print = function (value) {
+        console.log('Text: ' + value);
+    };
+    return Mytext2;
+}());
+var t2 = new Mytext2("Adan");
+t2.print('Test');
+var Car = (function () {
+    function Car(name, color, power) {
+        this.name = name;
+        this.color = color;
+        this.power = power;
+    }
+    ;
+    Car.prototype.getCar = function () {
+        return this.name;
+    };
+    Car.prototype.getColor = function () {
+        return this.color;
+    };
+    Car.prototype.getPower = function () {
+        return this.power;
+    };
+    return Car;
+}());
+var camaro = new Car("Camaro", "Yellow", 460);
+console.log(camaro.getCar(), camaro.getColor(), camaro.getPower());
--- main.ts
+++ main.ts
@@ -1,32 +1,68 @@
-// ---------------------------- Interface for functions
-interface iTypeFunc {
-    (a:number, b:number):boolean;
+interface iCountString {
+    valueTxt: string;
+    countTxt():number;
 }
 
-var add: iTypeFunc;
-add = function (varA:number, varB:number):boolean {
-    return true;
+class Mytext implements iCountString {
+    constructor(public valueTxt:string){};
+
+    countTxt():number {
+        return this.valueTxt.length;
+    };
 }
 
-// ------------------------------------------
-interface iColor {
-    (codeColor:string, title?:string):{codeColor:string, title?:string};
+var t = new Mytext("Adan Recarlos...");
+console.log(t.countTxt());
+
+// ---------------------------------- Duas Interfaces
+interface iPrintString {
+    print(value:string):void;
 }
 
-var getColor: iColor;
-getColor = function (codeColor:string, title?:string):{codeColor:string, title?:string} {
-    if(title)
-        return {codeColor:codeColor, title:title};
-    return {codeColor:codeColor};
+class Mytext2 implements iCountString, iPrintString {
+    constructor(public valueTxt:string){};
+
+    countTxt():number {
+        return this.valueTxt.length;
+    };
+
+    print(value:string):void {
+        console.log('Text: ' + value);
+    }
+}
+var t2 = new Mytext2("Adan");
+t2.print('Test');
+
+// ------------------------------ Herança
+interface iCarString {
+    name:string;
+    getCar():string;
 }
 
-console.log(getColor("#fff", "White"));
-
-// ------------------------------------------- Interface for array
-interface iArrayTypes {
-    [index:number]:string;
+interface iCarColor {
+    color:string;
+    getColor():string;
 }
 
-var a: iArrayTypes;
-a = ["test", 65];
-console.log(a);
+interface iCar extends iCarString, iCarColor{
+    power:number;
+    getPower():number;
+}
+
+class Car implements iCar {
+    constructor(public name: string, public color: string, public power: number){};
+
+    getCar():string {
+        return this.name;
+    }
+
+    getColor():string {
+        return this.color;
+    }
+
+    getPower():number {
+        return this.power;
+    }
+}
+var camaro = new Car("Camaro", "Yellow", 460);
+console.log(camaro.getCar(), camaro.getColor(), camaro.getPower());
