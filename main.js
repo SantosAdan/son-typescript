656e783952d84d33fa882e500438d33f116868b8
Adan Santos <santos.adaan@gmail.com>
2016-06-11 15:26:08 -0300

    Aula 2 - Tipos Básicos

--- main.js
+++ main.js
@@ -1,20 +1,37 @@
-function getHello(name) {
-    return "Hello " + name;
-}
-//var name = 45;
+var test = false;
+var id = 2;
+// ------------------------------------- Numbers
+var decimal = 5.5;
+var hex = 0xf00d;
+var binary = 2;
+var octal = 484;
+// ------------------------------------- Strings
 var name = "Adan";
-document.body.innerHTML = getHello(name);
-var Car = (function () {
-    function Car() {
-    }
-    return Car;
-}());
-var getCar = function (car) {
-    console.log(car.brand, car.name, car.power);
-};
-var Camaro = new Car();
-Camaro.brand = "Chevrolet";
-Camaro.name = "Camaro";
-//Camaro.age = 2016;
-Camaro.power = 45;
-getCar(Camaro);
+var car = 'Camaro';
+var text = "<p>Name: " + name + "</p>\n                               <p>Car: " + car + "</p>";
+document.body.innerHTML = text;
+// ------------------------------------- Array
+var fruits = ["Apple", "Watermelon", "Lemon"];
+console.log(fruits);
+var fruits = ["Apple", "Watermelon", "Lemon"];
+console.log(fruits);
+// ------------------------------------- Enum
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 1] = "Red";
+    Color[Color["Green"] = 2] = "Green";
+    Color[Color["Blue"] = 3] = "Blue";
+})(Color || (Color = {}));
+;
+console.log(Color.Red);
+console.log(Color[2]);
+// ------------------------------------- Any
+var list = 23;
+console.log(list);
+list = "String...";
+console.log(list);
+// ------------------------------------- Void
+function printNumber(num) {
+    alert(num);
+}
+printNumber(32);
--- main.ts
+++ main.ts
@@ -1,33 +1,40 @@
-function getHello(name: string) {
-    return "Hello " + name;
+
+var test: boolean = false;
+var id: number = 2;
+
+// ------------------------------------- Numbers
+var decimal: number = 5.5;
+var hex: number = 0xf00d;
+var binary: number = 0b10;
+var octal: number = 0o744;
+
+// ------------------------------------- Strings
+var name: string = "Adan";
+var car: string = 'Camaro';
+var text: string = `<p>Name: ${name}</p>
+                               <p>Car: ${car}</p>`;
+document.body.innerHTML = text;
+
+// ------------------------------------- Array
+var fruits: string[] = ["Apple", "Watermelon", "Lemon"];
+console.log(fruits);
+var fruits: Array<string> = ["Apple", "Watermelon", "Lemon"];
+console.log(fruits);
+
+// ------------------------------------- Enum
+enum Color {Red = 1, Green, Blue};
+console.log(Color.Red);
+console.log(Color[2]);
+
+// ------------------------------------- Any
+var list: any = 23;
+console.log(list);
+
+list = "String...";
+console.log(list);
+
+// ------------------------------------- Void
+function printNumber(num:number):void {
+    alert(num);
 }
-
-//var name = 45;
-var name = "Adan";
-
-document.body.innerHTML = getHello(name);
-
-interface iCar {
-    brand:string;
-    name:string;
-    power:number;
-}
-
-class Car {
-    brand:string;
-    name:string;
-    //age:number;
-    power:number;
-}
-
-var getCar = function(car:iCar) {
-    console.log(car.brand, car.name, car.power);
-};
-
-var Camaro = new Car();
-Camaro.brand = "Chevrolet";
-Camaro.name = "Camaro";
-//Camaro.age = 2016;
-Camaro.power = 45;
-
-getCar(Camaro);
+printNumber(32);
